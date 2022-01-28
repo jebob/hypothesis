932b0fcb180dc1ecd7205bf97bf902a02ab12f80
Robert Howlett <robert.howlett@auroraer.com>
2022-01-28 12:52:11 +0000

    Test for swarm approach to one_of
    I suspect this should fail

--- hypothesis-python/tests/cover/test_one_of.py
+++ hypothesis-python/tests/cover/test_one_of.py
@@ -15,7 +15,7 @@ import pytest
 from hypothesis import given, strategies as st
 from hypothesis.errors import InvalidArgument
 
-from tests.common.debug import assert_no_examples
+from tests.common.debug import assert_no_examples, find_any
 
 
 def test_one_of_empty():
@@ -46,3 +46,15 @@ def test_one_of_without_strategies_suggests_sampled_from():
         match=re.escape("Did you mean st.sampled_from([1, 2, 3])?"),
     ):
         st.one_of(1, 2, 3)
+
+
+def test_one_of_extreme_corner_case():
+    # Chance of drawing None {test_size} times in a row is 1/3^test_size if one_of just picks randomly,
+    # or ~1/7 if it uses three boolean toggles.
+    # Use three strategies to thwart the obvious test cases of MIN or MAX data.
+    test_size = 100
+    target = [None] * test_size
+    find_any(
+        st.lists(st.one_of(st.just(False), st.just(None), st.just(True)), max_size=test_size, min_size=test_size),
+        lambda x: x == target,
+    )
